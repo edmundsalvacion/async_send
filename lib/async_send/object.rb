3aeb16461aa5f32b2044357830387f18aeec4090
gilles <gilles@peerpong.com>
2010-12-17 17:47:22 -0800

    can work sync if no pool is defineds

--- lib/async_send/object.rb
+++ lib/async_send/object.rb
@@ -1,18 +1,24 @@
 module AsyncSend
   module Object
-    
+
     def async_send(method, *args)
 
-      if self.respond_to? method 
-        job = {
-          :class => self.class.to_s,
-          :id => self.id,
-          :args => args,
-          :method => method.to_s
-        }.to_json
+      if self.respond_to? method
 
-        AsyncSend.config.pool.use(AsyncSend.config.tube)
-        AsyncSend.config.pool.put(job)
+        if AsyncSend.config.pool.nil?
+          self.send(method, *args)
+        else
+          job = {
+            :class => self.class.to_s,
+            :id => self.id,
+            :args => args,
+            :method => method.to_s
+          }.to_json
+
+          AsyncSend.config.pool.use(AsyncSend.config.tube)
+          AsyncSend.config.pool.put(job)
+        end
+
       else
         # !!TODO: Handle invalid method
         puts 'Invalid method'
