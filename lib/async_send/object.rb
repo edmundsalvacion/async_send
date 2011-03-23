88ad742f7894677d0787f5f9d6cb6e881d690edb
Edmund Salvacion <edmund@schematiclabs.com>
2011-03-23 14:35:46 -0700

    Added async_send_with_delay

--- lib/async_send/object.rb
+++ lib/async_send/object.rb
@@ -2,9 +2,17 @@ module AsyncSend
   module Object
 
     def async_send(method, *args)
+      self.send_job(method, 0, *args)
+    end
 
+    def async_send_with_delay(method, delay, *args)
+      self.send_job(method, delay, *args)
+    end
+
+    private
+
+    def send_job(method, delay=0, *args)
       if self.respond_to? method
-
         if AsyncSend.config.pool.nil?
           self.send(method, *args)
         else
@@ -16,9 +24,8 @@ module AsyncSend
           }.to_json
 
           AsyncSend.config.pool.use(AsyncSend.config.tube)
-          AsyncSend.config.pool.put(job)
+          AsyncSend.config.pool.put(job, 65536, delay)
         end
-
       else
         raise NoMethodError "undefined method `#{method.to_s}' for #{self.to_s}"
       end
