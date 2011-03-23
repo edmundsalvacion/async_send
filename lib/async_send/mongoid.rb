6d8cd17a316e94bf30d1d08b018f9f44b7fc89b5
Edmund Salvacion <edmund@schematiclabs.com>
2011-03-23 16:15:48 -0700

    Delayed job support for mongoid
    * Added async_send_with_delay to mongoid document
    * Improved consistency with beanstalk bypass

--- lib/async_send/mongoid.rb
+++ lib/async_send/mongoid.rb
@@ -3,37 +3,48 @@ module AsyncSend
     module Document
 
       def async_send(method, *args)
+        self.send_job(method, 0, *args)
+      end
+
+      def async_send_with_delay(method, delay=0, *args)
+        self.send_job(method, delay, *args)
+      end
+
+      private
+
+      def send_job(method, delay=0, *args)
 
         if self.respond_to? method
+          job = {
+            :class => self.class.to_s,
+            :id => self.id,
+            :args => args,
+            :method => method.to_s
+          }
 
-          if AsyncSend.config.pool.nil?
-            self.send(method, *args)
-          else
-            job = {
-              :class => self.class.to_s,
-              :id => self.id,
-              :args => args,
-              :method => method.to_s
-            }
+          if self.embedded?
 
-            if self.embedded?
-
-              # get relation key
-              self._parent.relations.each_value do |relation|
-                is_embedded = (relation.macro.eql?(:embeds_one) or relation.macro.eql?(:embeds_many))
-                if relation.class_name.eql?(self.class.to_s) and is_embedded
-                  job[:relation_key] = relation.key
-                  break
-                end
+            # get relation key
+            self._parent.relations.each_value do |relation|
+              is_embedded = (relation.macro.eql?(:embeds_one) or relation.macro.eql?(:embeds_many))
+              if relation.class_name.eql?(self.class.to_s) and is_embedded
+                job[:relation_key] = relation.key
+                break
               end
-
-              job[:embedded] = true
-              job[:parent_id] = self._parent.id.to_s
-              job[:parent_class] = self._parent.class.to_s
             end
 
+            job[:embedded] = true
+            job[:parent_id] = self._parent.id.to_s
+            job[:parent_class] = self._parent.class.to_s
+          end
+
+
+          unless AsyncSend.config.pool.nil?
             AsyncSend.config.pool.use(AsyncSend.config.tube)
             AsyncSend.config.pool.put(job.to_json)
+          else
+            # Run job when no pool has been set
+            AsyncSend::Worker.run_job(job.to_json)
           end
 
         else
--- lib/async_send/worker.rb
+++ lib/async_send/worker.rb
@@ -22,21 +22,7 @@ module AsyncSend
         if job = AsyncSend.config.pool.reserve
           begin
             @busy = true
-            data = ActiveSupport::JSON.decode(job.body)
-
-            puts "Job Recieved #{Time.now}"
-            puts data
-            puts "\n"
-
-            unless data['embedded']
-              object = Kernel.const_get(data['class']).find(data['id'])
-            else
-              parent = Kernel.const_get(data['parent_class']).find(data['parent_id'])
-              object = parent.send(data['relation_key']).find(data['id'])
-            end
-
-            object.send(data['method'], *data['args'])
-
+            AsyncSend::Worker.run_job(job.body)
           rescue Exception => e
             puts "Job Error #{Time.now}"
             puts e.message
@@ -60,6 +46,23 @@ module AsyncSend
 
     def term?
       @term
+    end
+
+    def self.run_job(payload)
+      data = ActiveSupport::JSON.decode(payload)
+
+      puts "Job Recieved #{Time.now} ----------------------------"
+      puts data
+      puts "\n"
+
+      unless data['embedded']
+        object = Kernel.const_get(data['class']).find(data['id'])
+      else
+        parent = Kernel.const_get(data['parent_class']).find(data['parent_id'])
+        object = parent.send(data['relation_key']).find(data['id'])
+      end
+
+      object.send(data['method'], *data['args'])
     end
 
   end
